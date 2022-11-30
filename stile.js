2deb287932eb69318ded2704ac4f3c18cbafa62d
fransinarm <119495578+fransinarm@users.noreply.github.com>
2022-11-30 02:40:55 -0800

    Add files via upload

---
+++ stile.js
@@ -0,0 +1,9 @@
+document.writeln("<CENTER>");
+document.writeln("<H1>POLITEKNIK AMAMAPARE TIMIKA<H1>");
+document.writeln("<H2>FAKULTAS TEKNIK DAN KOMPUTER<H2>");
+document.writeln("<H3>JURUSAN REKAYASA INTRNET<H3>");
+document.writeln("<HR WiDTH=600 COLOR=BLACK>");
+document.writeln("</CENTER>");
+
+
+
